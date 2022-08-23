5a2816ebec718d77213ef57c5893525a88e75f4c
johnpascual <ashleypascual@hotmail.es>
2022-08-23 09:52:43 +0100

    Add initial config

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from './App';
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
