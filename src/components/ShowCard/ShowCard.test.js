7f52efe2a2ea53cdc82f89ba4fc7b2a4888f6648
johnpascual <ashleypascual@hotmail.es>
2022-08-23 16:55:15 +0100

    testing

---
+++ src/components/ShowCard/ShowCard.test.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ import { screen, render } from '@testing-library/react'
+ import '@testing-library/jest-dom'
+ import React from 'react'
+ 
+ import ShowCard from '.'
+
+ describe("ShowCard", () => {
+
+    beforeEach(() => {
+        render(<ShowCard data={{ summary: "Test description", name: "Test name" }}/>)
+    })
+
+    it("Displays the appropriate description", () => {
+        const description = screen.queryByRole("paragraph")
+        expect(description).toBeInTheDocument()
+        expect(description.textContent).toBe("Test description")
+    })
+
+    it("Doesn't display an image element when no image is provided.", () => {
+        const image = screen.queryByRole("img")
+        expect(image).not.toBeTruthy()
+    })
+
+})
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import React from "react";
-import ReactDOM from "react-dom/client";
 
 import App from './App';
 
